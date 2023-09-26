6ea4d0bc821ebaa016625174628d458243b4decf
Nazer Zahir <nazersemail@gmail.com>
2023-09-26 18:48:51 +0000

    saving progress 1.0

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,85 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.h2.command.Prepared;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+public class AccountDAO {
+    /*
+     * 
+     * Requirements:
+     *      register
+     *      login
+     * 
+     * 
+     * //
+     */
+    /* 
+     public Account getLogin(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Account temp = new Account(rs.getInt("account_id"),
+                rs.getString("username"),
+                rs.getString("password"));
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return null;
+    }
+
+    */
+    public Account getLogin(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "select * from account where username = ? AND password = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,account.getUsername());
+            ps.setString(2,account.getPassword());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Account temp = new Account(rs.getInt("account_id"),
+                rs.getString("username"),
+                rs.getString("password"));
+                return temp;
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return null;
+
+    }
+    public Account addUser(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "insert into account (account_id,username,password) values(?,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1,account.getAccount_id());
+            ps.setString(2,account.getUsername());
+            ps.setString(3,account.getPassword());
+            ps.executeUpdate();
+            return account;
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
+
---
+++ src/main/java/DAO/MessageDAO.java
@@ -0,0 +1,156 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Message;
+import Util.ConnectionUtil;
+
+public class MessageDAO {
+    /*//
+     * requirements: 
+     *      create message
+     *      Get all messages 
+     *      get message by id
+     *      delete message by message id
+     *      update message by message id
+     *      get message by user id 
+     *///
+       /* 
+     public Message getLogin(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Message temp = new Message(rs.getInt(""),
+                rs.getString(""),
+                rs.getString(""));
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return null;
+    }
+
+    */
+    public List<Message> getAllMessages(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try{
+            String sql = "select * from message";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                Message temp = new Message(rs.getInt("message_id"),
+                rs.getInt("posted_by"),
+                rs.getString("message_text"),
+                rs.getLong("time_posted_epoch"));
+                messages.add(temp);
+            }
+
+            return messages;
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    public Message addMessage(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "insert into message(posted_by,message_text,time_posted_epoch) values (?,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1,message.getPosted_by());
+            ps.setString(2,message.getMessage_text());
+            ps.setLong(3,message.getTime_posted_epoch());
+            ps.executeUpdate();
+            return message;
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return null;
+    }
+    public Message getMessageByMessageId(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "select * from message where message_id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1,message.getMessage_id());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Message temp = new Message(rs.getInt("message_id"),
+                rs.getInt("posted_by"),
+                rs.getString("message_text"),
+                rs.getLong("time_posted_epoch"));
+                return temp;
+            }
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    public List<Message> getAllMessagesByUser(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try{
+            String sql = "select * from message where posted_by = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1,message.getPosted_by());
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Message curr = new Message(rs.getInt("message_id"),
+                rs.getInt("posted_by"),
+                rs.getString("message_text"),
+                rs.getLong("time_posted_epoch"));
+                messages.add(curr);
+            }
+            return messages;
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    public Message deleteMessage(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "delete from message where message_id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, message.getMessage_id());
+            ps.executeUpdate();
+            return message;
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    public Message updateMessage(Message oldMessage, String newMessage){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "update message set message_text = ? where message_id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,newMessage);
+            ps.setInt(2,oldMessage.getMessage_id());
+            ps.executeUpdate();
+            oldMessage.setMessage_text(newMessage);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
